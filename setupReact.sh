77a808f7f84c7b1d40ca3eb3cf42ff6a1c7234be
Yamikani Chinamale <ychinamale@gmail.com>
2020-04-29 00:40:37 +0200

    Changed title to ReactJs App

--- setupReact.sh
+++ setupReact.sh
@@ -78,7 +78,7 @@ cat << indexhtml > ${HTML_TEMPLATE}
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Github Battle</title>
+    <title>ReactJs App</title>
 </head>
 <body>
     <div id="app"></div>
