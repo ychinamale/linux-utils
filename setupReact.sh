c9f0b42bf26beee7a4595a84a87032dc746eb2ac
Yamikani Chinamale <ychinamale@gmail.com>
2020-04-28 23:58:23 +0200

    Added css reset

--- setupReact.sh
+++ setupReact.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+CSS_RESET='src/reset.css'
 WEBPACK_CONFIG='webpack.config.js'
 ENTRY='src/index.js'
 JS_BUNDLE='bundle.js'
@@ -54,6 +55,7 @@ createSourceFiles () {
 cat <<indexjs > ${ENTRY}
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './reset.css';
 import './index.css';
 
 function App () {
@@ -87,6 +89,12 @@ indexhtml
 
     echo "Creating src/index.css ..."
     touch src/index.css
+
+
+    echo "Creating ${CSS_RESET} ..."
+cat <<resetcss > ${CSS_RESET}
+a,abbr,acronym,address,applet,article,aside,audio,b,big,blockquote,body,canvas,caption,center,cite,code,dd,del,details,dfn,div,dl,dt,em,embed,fieldset,figcaption,figure,footer,form,h1,h2,h3,h4,h5,h6,header,hgroup,html,i,iframe,img,ins,kbd,label,legend,li,mark,menu,nav,object,ol,output,p,pre,q,ruby,s,samp,section,small,span,strike,strong,sub,summary,sup,table,tbody,td,tfoot,th,thead,time,tr,tt,u,ul,var,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}article,aside,details,figcaption,figure,footer,header,hgroup,menu,nav,section{display:block}body{line-height:1}ol,ul{list-style:none}blockquote,q{quotes:none}blockquote:after,blockquote:before,q:after,q:before{content:'';content:none}table{border-collapse:collapse;border-spacing:0}
+resetcss
 }
 
 createProjectDir() {
